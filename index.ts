a5c16ca0aec3b839655382ecf58110af6bde7afc
Sigve Hansen <sighan12@gmail.com>
2023-02-28 15:02:44 +0100

    start work on composable

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-export const add = (a: number, b: number) => a + b
+import { reactive, computed, ref, Ref } from 'vue'
+
+type MaybeRef<T> = Ref<T> | undefined
+
+export interface UseDatepickerOptions {
+	/**
+	 * Date used when initializing the useDatepicker composable.  
+	 * @default new Date()
+	 */
+	initialDate?: MaybeRef<Date>
+}
+
+export function useDatepicker(options: UseDatepickerOptions = {}) {
+	const { initialDate = new Date() } = options
+
+	// Date
+	const currentDate = ref(initialDate.getDate())
+
+	return {
+		currentDate
+	}
+}
