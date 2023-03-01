9b6992d58dd79d94446532f1045d819de122de54
Sigve Hansen <sighan12@gmail.com>
2023-03-01 09:56:10 +0100

    build: fix build step

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
   extends: ['@sxzz/eslint-config'],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
 }
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,77 @@
-import { reactive, computed, ref, Ref } from 'vue'
+import { reactive, computed, ref, unref } from 'vue'
+import type { Ref } from 'vue'
 
-type MaybeRef<T> = Ref<T> | undefined
+type MaybeRef<T> = T | Ref<T>
 
 export interface UseDatepickerOptions {
 	/**
-	 * Date used when initializing the useDatepicker composable.  
+	 * Date used when initializing the useDatepicker composable.
+	 * 
 	 * @default new Date()
 	 */
 	initialDate?: MaybeRef<Date>
 }
 
-export function useDatepicker(options: UseDatepickerOptions = {}) {
-	const { initialDate = new Date() } = options
+/**
+ * Encapsulated logic for a functional datepicker
+ * 
+ * @param options 
+ * @returns 
+ */
+export function useDatepicker(initialDate: MaybeRef<Date> = new Date()) {
+	initialDate = unref(initialDate)
 
 	// Date
-	const currentDate = ref(initialDate.getDate())
+	const currentDate = ref<number>(initialDate.getDate())
+
+	function getDateProps(date: number) {
+		return reactive({
+			'aria-selected': date === currentDate.value
+		})
+	}
+
+	function getDateEvents(date: number) {
+		return {
+			click() {
+				currentDate.value = date
+			}
+		}
+	}
+
+
+	// Month
+	const currentMonth = ref<number>(initialDate.getMonth())
+
+	function previousMonth() {
+		currentMonth.value -= 1
+	}
+
+	function nextMonth() {
+		currentMonth.value += 1
+	}
+
+
+	// Year
+	const currentYear = ref<number>(initialDate.getFullYear())
+
+	function previousYear() {
+		currentYear.value -= 1
+	}
+
+	function nextYear() {
+		currentYear.value += 1
+	}
+
 
 	return {
-		currentDate
+		currentDate,
+		getDateProps,
+		getDateEvents,
+
+		currentMonth,
+
+		currentYear,
+		previousYear,
+		nextYear,
 	}
 }
